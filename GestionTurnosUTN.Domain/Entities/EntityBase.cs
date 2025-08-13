9b6c605dd00f3a537802e89952148af9873f753f
Herrera García Cristian Iván <Cristian.HerreraGarcia@alu.frt.utn.edu.ar>
2025-08-13 17:17:45 -0300

    agrego los archivos base

---
+++ GestionTurnosUTN.Domain/Entities/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTurnosUTN.Domain.Entities;
+
+public class EntityBase
+{
+    Guid ID { get; set; }
+    public EntityBase()
+    {
+        ID = Guid.NewGuid();
+    }
+}
---
+++ GestionTurnosUTN.Domain/Entities/TurnStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTurnosUTN.Domain.Entities
+{
+    public enum TurnStatus
+    {
+        PENDING,
+        ATTENDED,
+        LOST,
+        CANCELLED
+    }
+}

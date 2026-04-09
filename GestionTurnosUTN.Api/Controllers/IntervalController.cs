bed1edf04e38c61792e9bfe24e4359d0057815e7
Victor Libran <Victor.Libran@alu.frt.utn.edu.ar>
2026-04-09 12:34:51 -0300

    interval

---
+++ GestionTurnosUTN.Api/Controllers/IntervalController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace GestionTurnosUTN.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+    public class IntervalController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World");
+    }
+}
+
---
+++ GestionTurnosUTN.Application/Interfaces/IInterval.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTurnosUTN.Application.Interfaces
+{
+    internal class IInterval
+    {
+    }
+}

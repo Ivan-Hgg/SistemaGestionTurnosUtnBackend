4ac9fdf6140b8860d67af5d3a20d4b57ef4fcd1d
Carlos <CarlosLixKlett@alu.frt.utn.edu.ar>
2026-04-09 12:03:03 -0300

    agregue servicio de News y puse la bd en el appsettings.json

---
+++ GestionTurnosUTN.Api/Controllers/NewsController.cs
@@ -0,0 +1,47 @@
+﻿using GestionTurnosUTN.Application.Dtos;
+using GestionTurnosUTN.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionTurnosUTN.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NewsController : ControllerBase
+{
+    private readonly INewsService _newsService;
+
+    public NewsController(INewsService newsService)
+    {
+        _newsService = newsService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var news = await _newsService.GetAllNewsAsync();
+        return Ok(news);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] NewsModel.RequestNewsModel request)
+    {
+        // Por ahora hardcodeamos el workerId hasta implementar autenticación
+        var workerId = Guid.NewGuid();
+        await _newsService.CreateNewsAsync(request, workerId);
+        return Ok("Noticia creada exitosamente");
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] NewsModel.RequestNewsModel request)
+    {
+        await _newsService.UpdateNewsAsync(id, request);
+        return Ok("Noticia actualizada exitosamente");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _newsService.DeleteNewsAsync(id);
+        return Ok("Noticia eliminada exitosamente");
+    }
+}
---
+++ GestionTurnosUTN.Application/Interfaces/INewsService.cs
@@ -0,0 +1,17 @@
+﻿using GestionTurnosUTN.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTurnosUTN.Application.Interfaces
+{
+    public interface INewsService
+    {
+        Task CreateNewsAsync(NewsModel.RequestNewsModel request, Guid workerId);
+        Task UpdateNewsAsync(Guid newsId, NewsModel.RequestNewsModel request);  
+        Task DeleteNewsAsync(Guid newsId);                                       
+        Task<IEnumerable<NewsModel.ResponseNewsModel>> GetAllNewsAsync();
+    }
+}
